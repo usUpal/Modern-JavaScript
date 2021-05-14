6c0488a2ab2d527e6cc4f39a273a08fc73ff9cc3
usUpal <udyan.upal@northsouth.edu>
2021-05-15 04:24:26 +0600

    parents, children and siblings

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,12 @@
-const content = document.querySelector('p')
-console.log(content.classList);
-content.classList.add('success')
-content.classList.remove('success')
+const article = document.querySelector('article').children;
+for(let i =0; i<article.length;i++){
+    console.log(article[i])
+}
+for(let i =0; i<article.length;i++){
+    article[i].classList.add('article-element')
+}
+//finding the parent of a child
+const title = document.querySelector('h2')
+console.log(title.parentElement.parentElement.parentElement)
+console.log(title.nextElementSibling);
+console.log(title.previousElementSibling);
