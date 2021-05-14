7a7dbea78f9407994f22369d9b2bf761b5d6c675
usUpal <udyan.upal@northsouth.edu>
2021-05-15 04:35:31 +0600

    event listener basic

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,4 @@
-const article = document.querySelector('article').children;
-for(let i =0; i<article.length;i++){
-    console.log(article[i])
-}
-for(let i =0; i<article.length;i++){
-    article[i].classList.add('article-element')
-}
-//finding the parent of a child
-const title = document.querySelector('h2')
-console.log(title.parentElement.parentElement.parentElement)
-console.log(title.nextElementSibling);
-console.log(title.previousElementSibling);
+const button = document.querySelector('button')
+button.addEventListener('click',() =>{
+    console.log('u clicked')
+})
