658472bb900695adabcac85f16d61c20a9bc20f4
usUpal <udyan.upal@northsouth.edu>
2021-05-15 05:07:10 +0600

    todo via event listener 1.1

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,11 @@
+// const ul = document.querySelector('ul')
+// ul.remove()
 const items= document.querySelectorAll('li')
 
 items.forEach(item =>{
     item.addEventListener('click', e=>{
         console.log(`${item.innerText} done`)
-        e.target.style.textDecoration = 'line-through'
+        //e.target.style.textDecoration = 'line-through'
+        e.target.remove()
     })
 })
