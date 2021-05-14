0c0ccef5a166b9a1bb8e2f06175c9330e5491555
usUpal <udyan.upal@northsouth.edu>
2021-05-14 23:58:11 +0600

    queryselector

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,16 @@
-const para0 = document.querySelector('.hello')
-const para1 = document.querySelector('.lorem')
-const para2 = document.querySelector('.error')
-console.log(para0)
-console.log(para1)
-console.log(para2)
+// const para0 = document.querySelector('.hello')
+// const para1 = document.querySelector('.lorem')
+// const para2 = document.querySelector('.error')
+// const para3 = document.querySelector('div.error')
+// console.log(para0)
+// console.log(para1)
+// console.log(para2)     
+// console.log(para3)     
+const paras = document.querySelectorAll('p')
+const errors = document.querySelectorAll('.error')
+paras.forEach((para) => {
+    console.log(para);
+})
+errors.forEach((error) =>{
+    console.log(error);
+})
